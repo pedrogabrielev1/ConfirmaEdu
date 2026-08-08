e3a2803049cacde75ecef4d98d970cb84d1b9d68
Pedro Gabriel <pedrogabrielevangelista.m@gmail.com>
2026-08-07 22:13:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CONFIRMAEDU_CONFIG = {
-  SUPABASE_URL: "https://ettocqjrtgeelfsiovql.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ettocqjrtgeelfsiovql.supabase.co/rest/v1",
   SUPABASE_KEY: "sb_publishable_hQPDYq4YqJp5IwZoZaJuJQ_adJW2_bW",
 };
